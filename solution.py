8de2d4fd139265076404cde6b99c9fb7db502182
Anastasia <33247868+AnastasiaValova@users.noreply.github.com>
2023-04-12 21:24:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,12 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
 
-chat_id = 225497605 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
+def solution1(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    
-    a = x_cnt*((x_success+y_success)/(x_cnt+y_cnt))
-    b = y_cnt*((x_success+y_success)/(x_cnt+y_cnt))
-    X=((x_success-a)**2)/a + ((y_success-b)**2)/b
-   
-    if X < stats.chi2.ppf(0.03, 1):
-        return True
-    else:
-        return False
+    alpha = 0.03
+    stats, p_value = proportions_ztest(np.array([x_success, y_success]), np.array([x_cnt, y_cnt]), alternative = 'smaller')
+    return p_value < alpha
